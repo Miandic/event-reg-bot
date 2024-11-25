44f356c6a5fcfc1824f2f267089b0d5b5a8c2949
Miandic <misha.andreev.novo@gmail.com>
2024-11-25 12:41:08 +0300

    Redacting instead of sending new

--- handlers.py
+++ handlers.py
@@ -13,7 +13,7 @@ async def cmd_start(message: Message):
 
 @start_router.callback_query(F.data == 'Home')
 async def cmd_start(call: CallbackQuery):
-    await call.message.answer('Привет!\n\nВыбирай команду, к какое хочешь присоедениться и вперёд!\nПосмотреть состав команды можно, нажав на неё. С друзьями веселее!', reply_markup=start_kb(call.from_user.id))
+    await call.message.edit_text('Привет!\n\nВыбирай команду, к какое хочешь присоедениться и вперёд!\nПосмотреть состав команды можно, нажав на неё. С друзьями веселее!', reply_markup=start_kb(call.from_user.id))
 
 
 @start_router.callback_query(F.data.startswith('show_team_'))
@@ -21,5 +21,9 @@ async def show_team(call: CallbackQuery):
     team_id = int(call.data.replace('show_team_', ''))
     formatted_message = 'Места:\n\nЗанято: 0\nСвободно:9\nНа удержании:1'
 
-    await call.message.answer(formatted_message, reply_markup=team_kb(call.from_user.id, team_id))
+    await call.message.edit_text(formatted_message, reply_markup=team_kb(call.from_user.id, team_id))
 
+
+@start_router.callback_query(F.data == 'Admin')
+async def admin(call: CallbackQuery):
+    await call.message.edit_text('Я получил власть, которая и не снилась моему отцу!', reply_markup=admin_kb())
--- keyboards.py
+++ keyboards.py
@@ -4,10 +4,12 @@ from bot_i import admins, banned, is_team_full, user_in_team
 
 def start_kb(user_telegram_id: int):
     kb_list = []
-    for i in range(1, 11):
-        t = 'Команда ' + str(i)
-        callback = 'show_team_'+ str(i)
-        kb_list.append([InlineKeyboardButton(text=t, callback_data=callback)])
+    for i in range(1, 11, 2):
+        t1 = 'Команда ' + str(i)
+        t2 = 'Команда ' + str(i+1)
+        callback1 = 'show_team_'+ str(i)
+        callback2 = 'show_team_'+ str(i+1)
+        kb_list.append([InlineKeyboardButton(text=t1, callback_data=callback1), InlineKeyboardButton(text=t2, callback_data=callback2)])
     if user_telegram_id in admins:
         kb_list.append([InlineKeyboardButton(text="⚙️ Админ панель", callback_data='Admin')])
     keyboard = InlineKeyboardMarkup(inline_keyboard=kb_list)
@@ -26,6 +28,13 @@ def team_kb(user_telegram_id: int, team_id: int):
         callback_self = 'Self_' + str(user_telegram_id)
         kb_list.append([InlineKeyboardButton(text="Присоедениться самому", callback_data=callback_self)])
     
+    kb_list.append([InlineKeyboardButton(text="Вернуться назад", callback_data='Home')])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=kb_list)
+    return keyboard
+
+def admin_kb():
+    kb_list = []
+    kb_list.append([InlineKeyboardButton(text="Какой-то функционал ¯\_(ツ)_/¯", callback_data='Adminsmth')])
     kb_list.append([InlineKeyboardButton(text="Вернуться назад", callback_data='Home')])
     keyboard = InlineKeyboardMarkup(inline_keyboard=kb_list)
     return keyboard
